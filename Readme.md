63dff7d5a08b65015743b92583bb391756cdd26d
zermelozf <arnaud.rachez@gmail.com>
2011-11-14 13:16:44 +0900

    Initial commit.

---
+++ Readme.md
@@ -0,0 +1,3 @@
+#Enumerate a given grammar
+
+Small function correcting some problems with the nltk.parse.generate example.
---
+++ src/enumerate.py
@@ -0,0 +1,33 @@
+'''
+Created on Nov 10, 2011
+
+@author: arnaud
+'''
+
+def enumerate(grammar):
+    return _enumerate(grammar, [grammar.start()])
+
+def _enumerate(grammar, symbols):
+    enum = []
+    if len(symbols) == 1:
+        if isinstance(symbols[0], str):
+            enum.append(symbols[0])
+        else:
+            for prod in grammar.productions(lhs=symbols[0]):
+                enum.extend(_enumerate(grammar, prod.rhs()))
+    else:
+        for first_symbol in _enumerate(grammar, [symbols[0]]):
+            for other_symbols in _enumerate(grammar, symbols[1:]):
+                    enum.append(first_symbol + ' ' + other_symbols)
+    
+    return enum
+
+if __name__ == "__main__":
+    import nltk
+    
+    grammar = nltk.parse.load_parser('file:../grammars/simple.cfg').grammar()
+    
+    for sent in enumerate(grammar):
+        print sent
+    
+    
