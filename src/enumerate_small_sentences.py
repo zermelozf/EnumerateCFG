882a1db6fce1a6b2da68b4cf65c46aca1a7c3265
zermelozf <arnaud.rachez@gmail.com>
2011-12-13 15:52:06 +0900

    Added Hasegawa's correction.

--- src/enumerate_small_sentences.py
+++ src/enumerate_small_sentences.py
@@ -8,18 +8,20 @@ def enumerate(grammar, max_len):
     return _enumerate(grammar, [grammar.start()], max_len)
 
 def _enumerate(grammar, symbols, max_len):
+    if len(symbols) > max_len:
+        return []
     enum = []
     if len(symbols) == 1:
         if isinstance(symbols[0], str):
-            enum.append(symbols[0])
+            enum.append([symbols[0]])
         else:
             for prod in grammar.productions(lhs=symbols[0]):
-                if max_len > len(prod.rhs()):
+                if max_len >= len(prod.rhs()):
                     enum.extend(_enumerate(grammar, prod.rhs(), max_len))                    
     else:
-        for first_symbols in _enumerate(grammar, [symbols[0]], max_len):
-            for other_symbols in _enumerate(grammar, symbols[1:], max_len-len(first_symbols.split(' '))):
-                enum.append(first_symbols + ' ' + other_symbols)
+        for first_symbol in _enumerate(grammar, [symbols[0]], max_len):
+            for other_symbols in _enumerate(grammar, symbols[1:], max_len-len(first_symbol)):
+                enum.append(first_symbol + other_symbols)
     return enum
 
 if __name__ == "__main__":
@@ -27,11 +29,11 @@ if __name__ == "__main__":
     
     grammar = nltk.parse.load_parser('file:../grammars/tong.cfg').grammar()
     
-    sentences = enumerate(grammar, 7) 
+    sentences = enumerate(grammar, 4) 
     
     for sent in sentences:
         print sent
         
-    print len(sentences)
+    print len(sentences), max([len(s) for s in sentences])
 
             
--- src/main.py
+++ src/main.py
@@ -6,30 +6,20 @@ Created on Nov 15, 2011
 import nltk
 import random
 from enumerate_small_sentences import enumerate
+import cPickle as pickle
     
 grammar = nltk.parse.load_parser('file:../grammars/tong.cfg').grammar()
 
-max_len = 6
+max_len = 8
 f = open('../enum/tong'+str(max_len)+'words.enum','w')
 
 sentences = enumerate(grammar, max_len)
-for sent in sentences:  
-    f.write(sent+'\n')    
-
-print 'cat hits girl who sees .' in sentences
-print 'cat who girl hits lives .' in sentences
-
-#print sentences
+   
+pickle.dump(sentences, f)
 
 for i in [random.randint(0,len(sentences)-1) for k in range(10)]:
     print sentences[i]
 
 print ""
 print "Number of sentences:", len(sentences)    
-print "Max_lenght =", max([len(s.split(' ')) for s in sentences])
-
-#for k in range(10):
-#    f = open('../enum/set'+str(k)+'_maxlen'+str(max_len),'w')
-#    print "Creating set", k
-#    for l in range(10000):
-#        f.write(sentences.pop(random.randint(0,len(sentences)-1))+'\n')
+print "Max_lenght =", max([len(s) for s in sentences])
